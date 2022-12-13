759efb5daca9d156b502abf42ed7ede00dd90b8a
MaeveMadiganMM <mum20@cam.ac.uk>
2022-12-13 15:49:22 +0100

    plotting script for comparison of CMS data with axion signal

---
+++ code/plot_axion_data_comparison_frombinned.py
@@ -0,0 +1,124 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+colors = sns.color_palette("colorblind", 8)
+import math
+import csv 
+
+
+font = {'size'   : 14}
+import matplotlib
+matplotlib.rc('font', **font)
+
+
+def csv_reader(filename):
+    output = []
+    with open(filename, "r") as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            output.append(row)
+        csvfile.close()
+
+    return output
+
+
+def read_HEPdata_SM():
+    data = csv_reader('../../sm_nnlo/data/HEPData-ins1901295-v1-parton_abs_ttm.csv')
+    cms_data  = []
+    for item in data[9:24]:
+        cms_data.append(float(item[3]))
+
+    covdata = csv_reader('../../sm_nnlo/data/HEPData-ins1901295-v1-parton_abs_ttm_covariance.csv')
+    covmat = np.zeros(15*15).reshape(15,15)
+
+    covmatlist = []
+    count=0
+    for item in covdata[9:234]:
+        covmatlist.append(float(item[6]))
+
+    for i in range(15):
+        for j in range(15):
+            covmat[i,j] = covmatlist[count]
+            count+=1
+
+    return cms_data, covmat
+
+
+
+def axion_signal(ct, bins, BR, kfac, kfac_unc):
+    '''
+    Calculate SM + C^2*sigma + C^4*sigma
+    '''
+    axion_lin = np.divide(BR*np.loadtxt('../saved_axion_hist_frombinned/mtt_ct1p0_nnpdf4p0_alphaS_linear.txt',usecols=(2,),dtype=float),bins_width)
+    axion_quad = np.divide(BR*np.loadtxt('../saved_axion_hist_frombinned/mtt_ct1p0_nnpdf4p0_alphaS_quad.txt',usecols=(2,),dtype=float),bins_width)
+    sm = np.divide(BR*np.loadtxt('../../sm_lo/mtt_SM_ttbar_nnpdf4p0.txt',usecols=(0,),dtype=float), bins_width)
+    signal = kfac*(sm + axion_lin*ct**2 + axion_quad*ct**4)
+
+    return signal
+    
+
+
+
+
+if __name__=='__main__':
+    
+    ctvals = [0.0, 4.0, 10.0, 20.0]
+    BR = 0.2877
+    kfac = np.loadtxt('../../sm_kfac_notebooks/kfac_nnlo_lo_highstats.txt', dtype=float, usecols=(0,))
+    kfac_unc = np.loadtxt('../../sm_kfac_notebooks/kfac_nnlo_lo_highstats.txt', dtype=float, usecols=(1,))
+
+    #Get CMS data
+    bins_cms=np.array([250.0,400.0,480.0,560.0,640.0,720.0,800.0,900.0,1000.0,1150.0,1300.0,1500.0,1700.0,2000.0,2300,3500])
+    bins_cms_C = 0.5*(bins_cms[:-1] + bins_cms[1:])
+    bins_width = bins_cms[1:]-bins_cms[:-1]
+    cms_data, cms_covmat = read_HEPdata_SM();
+    cms_unc = list(np.sqrt(np.diag(cms_covmat)))
+
+    #Create arrays for step plot
+    cms_data_plot = np.concatenate([cms_data, [cms_data[-1]]])
+    cms_unc_plot = np.concatenate([cms_unc, [cms_unc[-1]]])
+
+    
+    #Create figure and axes
+    fig = plt.figure(figsize=(7, 7))
+    gs = fig.add_gridspec(2, 1, height_ratios=(4, 1),
+                      left=0.14, right=0.86, bottom=0.1, top=0.9,
+                      wspace=0.55, hspace=0.25)
+    
+    ax = fig.add_subplot(gs[0, 0])
+    ax_ratio = fig.add_subplot(gs[1, 0], sharex=ax)
+    
+    #Plot data
+    ax.step(bins_cms, cms_data_plot, where='post',label='CMS data (fig 19 (a) of 2108.02803) ', color=colors[0])
+    ax.fill_between(bins_cms, cms_data_plot-cms_unc_plot, cms_data_plot+cms_unc_plot, color=colors[0],step='post', alpha=0.2)
+    ax.set_title('CMS data compared to SM+ALP signal')
+    ax.set_yscale('log')
+    ax.set_xlim([355,3500]);
+    ax.set_xlabel(r'$m_{t \bar{t}}$ [GeV]')
+    ax.set_ylabel(r'$\frac{d \sigma}{d m_{t \bar{t}}}$')
+
+
+    ax_ratio.set_xlabel(r'$m_{t \bar{t}}$ [GeV]')
+    ax_ratio.set_ylabel('Ratio to data')
+    ax_ratio.hlines(xmin=355, xmax=4000, y=1.0,color='black')
+    
+    for ind in range(len(ctvals)):
+        axion_data = axion_signal(ctvals[ind], bins_cms, BR, kfac, kfac_unc)
+
+        #Create arrays for step plot
+        axion_data_plot = np.concatenate([axion_data, [axion_data[-1]]])
+
+        #Create ratio arrays
+        axion_data_ratio = np.divide(axion_data_plot,cms_data_plot)
+        data_ratio_unc = np.divide(cms_unc_plot,cms_data_plot)
+
+        ax.step(bins_cms, axion_data_plot, where='post',label=r'SM + ALP, $c_{t} = $'+str(ctvals[ind]), color=colors[ind+1])
+        ax_ratio.step(bins_cms, axion_data_ratio, where='post',color=colors[ind+1])
+    
+    ax_ratio.fill_between(bins_cms, 1-data_ratio_unc, 1+data_ratio_unc, color=colors[0],step='post', alpha=0.4)
+    ax.legend(fontsize=12);
+    plt.tight_layout()
+    plt.savefig('axion_data_CMS_ttbar.pdf')
+    plt.show()
+
+
