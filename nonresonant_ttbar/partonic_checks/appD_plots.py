482fd2db745d54e87e5a26ffd63fba7ed057433b
MaeveMadiganMM <mum20@cam.ac.uk>
2023-03-17 09:28:00 +0000

    Update partonic checks plots

--- nonresonant_ttbar/partonic_checks/appD_plots.py
+++ nonresonant_ttbar/partonic_checks/appD_plots.py
@@ -5,6 +5,7 @@ import seaborn as sns
 colors = sns.color_palette("colorblind",8)
 color1 = colors[0]
 color2 = colors[4]
+color3 = colors[2]
 
 #Load Quad stuff
 roots = np.loadtxt('madgraph_cards/ttbar_gg_NR_quad_partonic/mtt_s_partonic_meframe_130323_quad.txt',dtype=float,usecols=(0,))/1000
@@ -47,10 +48,23 @@ funcSM = []
 for sv in rootsSM:
     funcSM.append(functionFitSM(1000*sv))
 
+
+
+
+def functionFitSMFull(x):
+    return 989753.0*((-7 + 8*np.log(0.0057971*x))/x**2 + 29756.3*(-17 + 32*np.log(0.0057971*x))/x**4)
+
+
+funcSMFull = []
+for sv in rootsSM:
+    funcSMFull.append(functionFitSMFull(1000*sv))
+
+
+
 #Quad only plot
 plt.figure()
 plt.plot(roots,func,color=color2,label=r'$\hat{\sigma}_{\mathrm{ALP}} \sim 1-2m_t^2/\hat{s}$')
-plt.plot(roots, sigma, color=color1, marker='o', linestyle=' ', ms=3, label=r'Madgraph')
+plt.plot(roots, sigma, color=color1, marker='o', linestyle=' ', ms=4, label=r'Madgraph')
 #plt.title(r'ALP contribution to $g g \rightarrow a \rightarrow t \bar{t}$')
 plt.xlabel(r'$\sqrt{\hat{s}}$ [TeV]', fontsize=12)
 plt.ylabel(r'$\hat{\sigma}_{\mathrm{ALP}}$ [pb]', fontsize=12)
@@ -62,8 +76,8 @@ plt.savefig('gg_ax_tt.pdf')
 
 #Lin only plot
 plt.figure()
-plt.plot(rootsLin,funcLin,color=color2, label=r'$\hat{\sigma}_{\mathrm{SM-ALP}} \sim \frac{1}{\hat{s}} \mathrm{Log}(\frac{\sqrt{\hat{s}}}{m_t})$')
-plt.plot(rootsLin, sigmaLin, color=color1,marker='o',linestyle=' ', ms=3, label='Madgraph')
+plt.plot(rootsLin,funcLin,color=color2, label=r'$\hat{\sigma}_{\mathrm{SM-ALP}} \sim \frac{1}{\hat{s}} \mathrm{Log}(\sqrt{\frac{\hat{s}}{m_t^2}})$')
+plt.plot(rootsLin, sigmaLin, color=color1,marker='o',linestyle=' ', ms=4, label='Madgraph')
 #plt.title('Interfernce term, partonic cross section')
 plt.xlabel(r'$\sqrt{\hat{s}}$ [TeV]',fontsize=12)
 plt.ylabel(r'$\hat{\sigma}_{\mathrm{SM-ALP}}$ [pb]', fontsize=12)
@@ -74,8 +88,9 @@ plt.savefig('gg_ax_tt_lin.pdf')
 
 #SM only plot
 plt.figure()
-plt.plot(rootsSM,funcSM,color=color2, label=r'$\hat{\sigma}_{\mathrm{SM}} \sim \frac{1}{\hat{s}} \mathrm{Log}(\frac{\sqrt{\hat{s}}}{m_t})$')
-plt.plot(rootsSM, sigmaSM,color=color1,marker='o',linestyle=' ', ms=3, label='Madgraph')
+plt.plot(rootsSM,funcSM,color=color2, label=r'$\hat{\sigma}_{\mathrm{SM}} \sim \frac{1}{\hat{s}} \mathrm{Log}(\sqrt{\frac{\hat{s}}{m_t^2}})$')
+plt.plot(rootsSM,funcSMFull,color=color3, label=r'Full $\hat{\sigma}_{\mathrm{SM}}$')
+plt.plot(rootsSM, sigmaSM,color=color1,marker='o',linestyle=' ', ms=4, label='Madgraph')
 #plt.title('SM term, partonic cross section')
 plt.xlabel(r'$\sqrt{\hat{s}}$ [TeV]', fontsize=12)
 plt.ylabel(r'$\hat{\sigma}_{\mathrm{SM}}$ [pb]', fontsize=12)
